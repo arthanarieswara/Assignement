163be35c59a240db6becf16c79efa652d2ee9f33
artanarieswara <arthanarieswara44@gmail.com>
2024-02-20 08:03:48 +0530

    first push

--- demo.js
+++ demo.js
@@ -1,21 +1,4 @@
-// Getting input via STDIN
-const readline = require("readline");
-
-const inp = readline.createInterface({
-  input: process.stdin
-});
-
-const userInput = [];
-
-inp.on("line", (data) => {
-  userInput.push(data);
-});
-
-inp.on("close", () => {
-  //start-here
-  //Your code goes here … replace the below line with your code logic 
-
-  console.log(userInput);
-
-  //end-here
-});
+name = "Arthanarieswara V"
+age = 24
+phone = 7708333036
+console.log(name, age, phone)
