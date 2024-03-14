d08f2d780c7add1b426e523d87db04ec33560829
artanarieswara <arthanarieswara44@gmail.com>
2024-03-14 07:23:16 +0530

    script.js alignment

--- Task 10/script.js
+++ Task 10/script.js
@@ -1,83 +1,83 @@
 //CALLBACK HELL FUNCTION
 
 let count = 11;
-setTimeout(() =>{
+setTimeout(() => {
+  count--;
+  console.log(count);
+  var screen = document.querySelector("#countdown");
+  screen.innerText = count;
+
+  setTimeout(() => {
     count--;
     console.log(count);
-    var screen = document.querySelector('#countdown');
+    var screen = document.querySelector("#countdown");
     screen.innerText = count;
-    
-    setTimeout(() =>{
+
+    setTimeout(() => {
+      count--;
+      console.log(count);
+      var screen = document.querySelector("#countdown");
+      screen.innerText = count;
+
+      setTimeout(() => {
         count--;
         console.log(count);
-        var screen = document.querySelector('#countdown');
+        var screen = document.querySelector("#countdown");
         screen.innerText = count;
-    
-        setTimeout(() =>{
+
+        setTimeout(() => {
+          count--;
+          console.log(count);
+          var screen = document.querySelector("#countdown");
+          screen.innerText = count;
+
+          setTimeout(() => {
             count--;
             console.log(count);
-            var screen = document.querySelector('#countdown');
+            var screen = document.querySelector("#countdown");
             screen.innerText = count;
-    
-            setTimeout(() =>{
+
+            setTimeout(() => {
+              count--;
+              console.log(count);
+              var screen = document.querySelector("#countdown");
+              screen.innerText = count;
+
+              setTimeout(() => {
                 count--;
                 console.log(count);
-                var screen = document.querySelector('#countdown');
+                var screen = document.querySelector("#countdown");
                 screen.innerText = count;
-    
-                setTimeout(() =>{
+
+                setTimeout(() => {
+                  count--;
+                  console.log(count);
+                  var screen = document.querySelector("#countdown");
+                  screen.innerText = count;
+
+                  setTimeout(() => {
                     count--;
                     console.log(count);
-                    var screen = document.querySelector('#countdown');
+                    var screen = document.querySelector("#countdown");
                     screen.innerText = count;
-    
-                    setTimeout(() =>{
-                        count--;
-                        console.log(count);
-                        var screen = document.querySelector('#countdown');
-                        screen.innerText = count;
-    
-                        setTimeout(() =>{
-                            count--;
-                            console.log(count);
-                            var screen = document.querySelector('#countdown');
-                            screen.innerText = count;
-    
-                            setTimeout(() =>{
-                                count--;
-                                console.log(count);
-                                var screen = document.querySelector('#countdown');
-                                screen.innerText = count;
-    
-                                setTimeout(() =>{
-                                    count--;
-                                    console.log(count);
-                                    var screen = document.querySelector('#countdown');
-                                    screen.innerText = count;
-    
-                                    setTimeout(() =>{
-                                        count--;
-                                        console.log(count);
-                                        var screen = document.querySelector('#countdown');
-                                        screen.innerText = count;
-    
-                                        setTimeout(() =>{
-                                            count--;
-                                            console.log(count);
-                                            var screen = document.querySelector('#countdown');
-                                            screen.innerText = count;
-                                            clearTimeout();
-                                            console.log("Happy Independence Day");
-                                            var screen = document.querySelector('#countdown');
-                                            screen.innerText = "Happy Independence Day!!!"
-                                        },1000)
-                                    },1000)
-                                },1000)
-                            },1000)
-                        },1000)
-                    },1000)
-                },1000)
-            },1000)
-        },1000)
-    },1000)
-},1000)
+
+                    setTimeout(() => {
+                      count--;
+                      console.log(count);
+                      var screen = document.querySelector("#countdown");
+                      screen.innerText = count;
+                      clearTimeout();
+                      console.log("Happy Independence Day");
+                      var screen = document.querySelector("#countdown");
+                      screen.innerText = "Happy Independence Day!!!";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
